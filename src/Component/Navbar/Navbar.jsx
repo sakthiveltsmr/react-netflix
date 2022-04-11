f48c22cac70d81c8a7984a3daa9ac7080dce32b2
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-04-11 20:56:02 +0530

    nav

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Notifications, Search } from "@material-ui/icons";
+import { ArrowDropDown, Notifications, Search } from "@material-ui/icons";
 import React from "react";
 import "./Navbar.css";
 
@@ -25,6 +25,13 @@ export const Navbar = () => {
             src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
             alt=""
           />
+          <div className="profile">
+            <ArrowDropDown className="icon" />
+            <div className="options">
+              <span>Settings</span>
+              <span>Logout</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
