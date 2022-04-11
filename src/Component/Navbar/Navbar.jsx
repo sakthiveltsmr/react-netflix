9d6f6ce0b7ff1a528119c64c8935bddea8f5aa10
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-04-11 20:59:58 +0530

    Navbar

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ export const Navbar = () => {
   return (
     <div className="navbar">
       <div className="container">
-        <div className="lef">
+        <div className="left">
           <img
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
             alt="netflix"
