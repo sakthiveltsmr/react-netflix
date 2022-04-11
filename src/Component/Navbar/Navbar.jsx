961168693b51a549987462a118be352bba954fa6
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-04-11 20:34:53 +0530

    Navbar

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -2,5 +2,12 @@ import React from "react";
 import "./Navbar.css";
 
 export const Navbar = () => {
-  return <div className="navbar">Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="container">
+        <div className="lef"></div>
+        <div className="right"></div>
+      </div>
+    </div>
+  );
 };
