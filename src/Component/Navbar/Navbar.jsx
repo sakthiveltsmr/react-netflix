d098534c9d0e947f71b3b8863c01ac9f52a74148
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-04-12 10:10:51 +0530

    Navbar

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -1,15 +1,22 @@
 import { ArrowDropDown, Notifications, Search } from "@material-ui/icons";
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.scss";
 
 export const Navbar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  window.onscroll = () => {
+    setIsScrolled(window.pageYOffset === 0 ? false : true);
+    return () => (window.onscroll = null);
+  };
+  console.log(isScrolled);
   return (
-    <div className="navbar">
+    <div className={isScrolled ? "navbar scrolled" : "navbar"}>
       <div className="container">
         <div className="left">
           <img
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
-            alt="netflix"
+            alt=""
           />
           <span>Homepage</span>
           <span>Series</span>
