f668c7cbb2ecf5746ac2f58397baca628822ce9a
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-04-13 20:48:44 +0530

    app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import Home from "./Component/Home/Home";
-
+import Register from "./Pages/Register/Register";
 function App() {
   return (
     <div className="App">
       <Home />
+      <Register />
     </div>
   );
 }
