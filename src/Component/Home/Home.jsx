e3ab369f1b666878c7faec24fb9ffaa9703bba7a
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-04-15 12:02:36 +0530

    listitem

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,12 +1,14 @@
 import { Navbar } from "../Navbar/Navbar";
 import "./Home.scss";
 import Features from "../Features/Features";
+import List from "../List/List";
 
 const Home = () => {
   return (
     <div className="home">
       <Navbar />
       <Features />
+      <List />
     </div>
   );
 };
--- src/Component/ListItem/ListItem.jsx
+++ src/Component/ListItem/ListItem.jsx
@@ -19,7 +19,7 @@ export default function ListItem({ index }) {
       onMouseLeave={() => setIsHovered(false)}
     >
       <img
-        src="https://occ-0-1723-92.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABU7D36jL6KiLG1xI8Xg_cZK-hYQj1L8yRxbQuB0rcLCnAk8AhEK5EM83QI71bRHUm0qOYxonD88gaThgDaPu7NuUfRg.jpg?r=4ee"
+        src="https://static1.colliderimages.com/wordpress/wp-content/uploads/2021/07/iron-man-trilogy.jpg"
         alt=""
       />
       {isHovered && (
@@ -38,8 +38,8 @@ export default function ListItem({ index }) {
               <span>1999</span>
             </div>
             <div className="desc">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-              Praesentium hic rem eveniet error possimus, neque ex doloribus.
+              Iron Man is a 2008 American superhero film based on the Marvel
+              Comics character of the same name.
             </div>
             <div className="genre">Action</div>
           </div>
